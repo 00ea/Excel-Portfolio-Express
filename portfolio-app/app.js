974fbabfa4a56fffc2d362b764f37fe8eb24641e
00ea <excelaibangbee@gmail.com>
2025-11-06 20:11:05 -0500

    6th commit
    - added some more images
    - finished majority of the individual pages
    - added a little bit of css to style the footer
    - worked a lot on the footer and headers using navbar
    - got the headers and footers to be dynamic and show no matter the page
    - struggling with getting individual page content to show
    - moved all the pages into pages folder for better organization

--- portfolio-app/app.js
+++ portfolio-app/app.js
@@ -17,6 +17,22 @@ app.set('layout', 'layout');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
